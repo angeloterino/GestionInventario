f6852dad3c1433a5ede4bb7f6ddd696880c1e12e
Angel Oterino <angel_oterino@hotmail.com>
2018-11-13 23:55:02 +0100

    Preliminar

---
+++ GestionInventario.Data/Models.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionInventario.Data.Models
+{
+    public class Element
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string ExpireDate { get; set; }
+        public int Quantity { get; set; }
+    }
+}
